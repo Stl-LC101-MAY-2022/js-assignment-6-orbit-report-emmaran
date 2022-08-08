af242d5964fbf7a07f5ad19ce22a1de40416cb4a
Emma Randolph <emma@Emmas-MBP-2.attlocal.net>
2022-08-08 17:49:36 -0500

    Updated Jasmine

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { count } from 'console';
+// import { count } from 'console';
 import { Satellite } from '../satellite';
 
 @Component({
